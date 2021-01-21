5dc76edc44686234cd468693c69b549b1a4acc91
ergün yılmaz <ergunyilmaz176@gmail.com>
2021-01-21 14:36:54 +0300

    Add project files.

---
+++ BLL/Abstract/IProductService.cs
@@ -0,0 +1,30 @@
+﻿using DAL.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace BLL.Abstract
+{
+    public interface IProductService
+    {
+     
+        List<Product> GetActive();
+
+       
+        List<Product> GetDefault(Expression<Func<Product, bool>> exp);
+       
+        void Add(Product Product);
+      
+        void Update(Product Product);
+        
+        void Remove(Guid id);
+        
+        Product GetById(Guid id);
+
+        
+        void RemoveAll(Expression<Func<Product, bool>> exp);
+
+        bool Any(Expression<Func<Product, bool>> exp);
+    }
+}
